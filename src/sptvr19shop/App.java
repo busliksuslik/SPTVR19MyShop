8688bb9185175bc715beec665695cf60cfee5b16
Oleg <oleg.harlamov@ivkhk.ee>
2020-10-07 08:30:27 +0300

    work ver

--- src/sptvr19shop/App.java
+++ src/sptvr19shop/App.java
@@ -18,13 +18,15 @@ class App {
     private boolean exit = true;
     public void run(){
         do{
-            System.out.printf("Add product - 1%nSell product - 2%nShow All products - 3%nExit - 4%n");
+            System.out.println("--------------------------");
+            System.out.printf("Add product - 1%nSell product - 2%nShow All products - 3%nExit - 4%nIncert: ");
             int ch = scan.nextInt();
             switch(ch){
-                case 1:
+                case 1:{
                     addProduct();
                     break;
-                case 2:
+                }
+                case 2:{
                     System.out.println("Insert product ID");
                     int id = scan.nextInt();
                     if (goods[id] == null){
@@ -34,28 +36,30 @@ class App {
                     int num = scan.nextInt();
                     goods[id].count-=num;
                     break;
-                case 3:
-                    showGoods();
+                }
+                case 3:{
+                    for (int i = 0 ; i < this.goods.length ; i++) {
+                        if (goods[i] == null) {
+                            continue;
+                        }
+                        System.out.println("-------------------------");
+                        System.out.println("Name - " + goods[i].name);
+                        System.out.println("Price - " + goods[i].price);
+                        System.out.println("Count - " + goods[i].count);
+                        System.out.println("ID - " + i);
+                    }
                     break;
-                case 4:
+                }
+                case 4:{
                     exit = false;
+                }
 
             }
         }while(exit);
     }
-    private void showGoods(){
-        for (Goods good : this.goods) {
-            if (good != null) {
-                System.out.println("-------------------------");
-                System.out.println("Name - " + good.name);
-                System.out.println("Price - " + good.price);
-                System.out.println("Count - " + good.count);
-            }
-        }
-    }
     private void addProduct(){
-        for (Goods good : this.goods) {
-            if (good == null) {
+        for (int i = 0 ; i < this.goods.length ; i++) {
+            if (goods[i] == null) {
                 System.out.println("Insert name");
                 scan.nextLine();
                 String name = scan.nextLine();
@@ -63,7 +67,7 @@ class App {
                 int price = scan.nextInt();
                 System.out.println("Insert number");
                 int count = scan.nextInt();
-                good = new Goods(name,price,count);
+                goods[i] = new Goods(name,price,count);
                 break;
             }
         }
